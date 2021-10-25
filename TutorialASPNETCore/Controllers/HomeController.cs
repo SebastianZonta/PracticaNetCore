d33bf78e1e892a9e96c7c7f7e29b77e4984e1fcd
Sebastian Zonta <seba.zonta@gmail.com>
2021-10-24 21:34:09 -0300

    Initial Commit

---
+++ TutorialASPNETCore/Controllers/HomeController.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using TutorialASPNETCore.Models;
+using TutorialASPNETCore.Repositories;
+
+namespace TutorialASPNETCore.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly IEmployeeRepository _employeeRepository;
+
+        public HomeController(IEmployeeRepository employeeRepository)
+        {
+            _employeeRepository = employeeRepository;
+           
+        }
+        public string Index()
+        {
+           return _employeeRepository.getEmployee(1).name;
+        }
+        public IActionResult Details()
+        {
+            Employee employee = _employeeRepository.getEmployee(1);
+            return View(employee);
+        }
+    }
+}
---
+++ TutorialASPNETCore/Repositories/EmployeeRepository.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using TutorialASPNETCore.Models;
+
+namespace TutorialASPNETCore.Repositories
+{
+    public class EmployeeRepository : IEmployeeRepository
+    {
+        private List<Employee> _employees;
+        public EmployeeRepository()
+        {
+            _employees = new List<Employee>()
+            {
+                new Employee()
+                {
+                    id = 1,
+                    email = "seba.zonta@gmail.com",
+                    name = "seba"
+                }
+            };
+
+        }
+        public Employee getEmployee(int id)
+        {
+            return _employees.FirstOrDefault(e => e.id == id);   
+        }
+    }
+      
+    
+}
