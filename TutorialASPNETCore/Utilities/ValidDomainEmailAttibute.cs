e6f1b003c562c7e095423ffc58d29c9e2c8f0aad
Sebastian Zonta <seba.zonta@gmail.com>
2021-12-21 17:22:36 -0300

    agregado jquery

---
+++ TutorialASPNETCore/Utilities/ValidDomainEmailAttibute.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace TutorialASPNETCore.Utilities
+{
+    public class ValidDomainEmailAttibute : ValidationAttribute
+    {
+        public override bool IsValid(object value)
+        {
+            return base.IsValid(value);
+        }
+    }
+}
